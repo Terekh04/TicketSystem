408602b4c149e7975d1737f2139425a6301f0c35
Tereh04 <dmiterekh04@gmail.com>
2025-05-01 02:27:35 +0500

    another get

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,14 @@
 import { useEffect } from 'react';
 import ListGroup from "./components/ListGroup";
+import RedirectButton from './components/RedirectButton';
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
 function App() {
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-      console.log("Запрос к:", `${apiUrl}/users/`);
-
-        const response = await fetch(`${apiUrl}/users/`, {
-          method: 'GET',
-          credentials: 'include', // Если нужны куки/авторизация
-          headers: {
-            'Content-Type': 'application/json',
-            // Добавьте другие заголовки при необходимости
-            // 'Authorization': 'Bearer your-token',
-          },
-        });
-
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const data = await response.json();
-        console.log("Получено: ", data);
-      } catch (error) {
-        console.error("Ошибка при запросе: ", error);
-      }
-      try {
-        const response = await fetch(`${apiUrl}/users/`);
-        
-        if (!response.ok) {
-          throw new Error(`Ошибка HTTP: ${response.status}`);
-        }
-      
-        const contentType = response.headers.get('content-type');
-        if (!contentType?.includes('application/json')) {
-          throw new Error('Ответ не в формате JSON!');
-        }
-      
-        const data = await response.json();
-        console.log("Данные пользователей:", data);
-      
-      } catch (error) {
-        console.error("Ошибка запроса:", error);
-      }
-    };
-
-    fetchData();
-  }, []); // Пустой массив зависимостей означает, что эффект выполнится один раз при монтировании
-
   return (
-    <div>
-      <ListGroup />
-    </div>
+    <>
+      <RedirectButton />
+    </>
   );
 }
 
---
+++ src/components/RedirectButton.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const apiUrl = import.meta.env.VITE_API_URL;
+
+const RedirectButton = () => {
+  const handleClick = () => {
+    window.location.href = '${apiUrl}/auth/google'; // Переход по URL
+  };
+
+  return (
+    <button onClick={handleClick}>
+      Перейти на site/auth/me
+    </button>
+  );
+};
+
+export default RedirectButton;
