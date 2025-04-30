c2d0192e3c25bef27f3ff8a23d52b4b1dcd0fdb1
Tereh04 <dmiterekh04@gmail.com>
2025-04-30 23:59:20 +0500

    bugs

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,25 +1,25 @@
 import { Fragment } from "react/jsx-runtime";
 
 function ListGroup() {
-    const items = [
-        'New York',
-        'San Francisco',
-        'Tokyo',
-        'Almighty',
-        'Serikbol'
-    ];
+  const items = ["New York", "San Francisco", "Tokyo", "Almighty", "Serikbol"];
 
-    return (
-        <>
-            <h1>List</h1>
-            {items.length===0 && <p>No item found</p>}
-            <ul className="list-group">
-                {items.map((item) => (
-                    <li className='list-group-item'key={item} onClick={()=> console.log('Clicked')}>{item}</li>
-            ))}
-            </ul>
-        </>
-    );
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={() => console.log("item")}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
