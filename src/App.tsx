2af6002ee07f08f769ea2fd13425452a956a27d9
Tereh04 <dmiterekh04@gmail.com>
2025-05-01 00:39:45 +0500

    first try of api

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
 import ListGroup from "./components/ListGroup";
+
+const apiUrl = import.meta.env.VITE_API_URL;
+
+fetch(`${apiUrl}/auth/me`)
+  .then(res => res.json())
+  .then(data => console.log("Получено: ", data));
+
+
 function App() {
   return (
     <div>
