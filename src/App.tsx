aa1110dd7d61fb7d0c6f13128b79a35b7be9c725
Tereh04 <dmiterekh04@gmail.com>
2025-05-02 23:14:14 +0500

    v1.3.2(Render problems)

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,32 @@ import { useEffect,useState } from 'react';
 import ChatBot from './services/ChatBot';
 import MainPage from './pages/MainPage';
 import GetOAuthToken from './services/GetOAuthToken';
+import { API_BASE } from './api';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
 
 
 function App() {
-  
   const [user, setUser] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    GetOAuthToken().then(setUser);
-  }, [])
+    fetch(`${API_BASE}/auth/me`, {
+      credentials: "include",
+    })
+      .then((res) => res.ok ? res.json() : null)
+      .then((user) => {
+        if (user) {
+          setUser(user);
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return <div style={{ textAlign: "center", marginTop: "100px" }}>Loading...</div>;
+  }
+
   return (
     <Router>
       <Routes>
