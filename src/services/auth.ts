ab2165dcd2e6519622e0a107724ced5cc9c80794
Tereh04 <dmiterekh04@gmail.com>
2025-05-03 21:16:22 +0500

    v1.3.4 (Vercel now)

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,6 +1,4 @@
-const API_URL    = import.meta.env.VITE_API_URL
-const API_PREFIX = import.meta.env.VITE_API_PREFIX || ''
-
+const API_URL = import.meta.env.VITE_API_URL;
 
 export interface User {
   id: number;
@@ -10,13 +8,15 @@ export interface User {
 }
 
 export function loginWithGoogle() {
-  window.location.href = `${API_URL}${API_PREFIX}/auth/google`
+  // В dev → "https://ticketsystem-qfj9.onrender.com/auth/google"
+  // В prod →   "/api/auth/google"
+  window.location.href = `${API_URL}/auth/google`;
 }
 
 export async function getCurrentUser(): Promise<User | null> {
-  const res = await fetch(`${API_URL}${API_PREFIX}/auth/me`, {
+  const res = await fetch(`${API_URL}/auth/me`, {
     credentials: 'include',
-  })
-  if (!res.ok) return null
-  return res.json()
+  });
+  if (!res.ok) return null;
+  return res.json();
 }
