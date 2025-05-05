188d2009e052d69736bd50af3a7aa1619f15fa11
Tereh04 <dmiterekh04@gmail.com>
2025-05-05 22:09:24 +0500

    v1.3.6(Google redirect practice)

--- src/App.tsx
+++ src/App.tsx
@@ -35,9 +35,7 @@ export default function App() {
         <Route
           path="/chat"
           element={
-            user
-              ? <ChatBot />
-              : <RedirectToGoogle />
+            <ChatBot />
           }
         />
         <Route
